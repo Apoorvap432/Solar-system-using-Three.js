5e1002713a465763a7c9c0a18152d7e09a669f85
Apoorva Panwar <apoorvapanwar488@gmail.com>
2025-07-15 18:42:28 +0530

    app.jsx setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 function App() {
+  const [speeds, setSpeeds] = useState({
+    mercury: 1,
+    venus: 0.8,
+    earth: 0.6,
+    mars: 0.5,
+    jupiter: 0.3,
+    saturn: 0.2,
+    uranus: 0.1,
+    neptune: 0.05,
+  });
 
   return (
-    <>
-      
-    </>
+    <div className="app">
+      <ControlPanel speeds={speeds} setSpeeds={setSpeeds} />
+      <SolarSystem speeds={speeds} />
+    </div>
   )
 }
 
